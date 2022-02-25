62fa449c8ef888b53aa06dcde854f6fc5bcf18fa
Tobiasz <tobikzak@gmail.com>
2022-02-25 11:31:11 +0100

    v2

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -7,6 +7,10 @@ class HelloJava {
 	public void afficherCancan(){
 
 	}
+	/**
+	 * Le main qui imprime HELLO BLAGNAC
+	 * @param args
+	 */	
 	public static void main(String[] args) {
 		System.out.println("Hello Blagnac");
 	}
