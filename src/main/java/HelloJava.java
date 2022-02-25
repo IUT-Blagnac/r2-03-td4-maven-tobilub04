4d3d175b9b85505b319ded84fd095f0efe2b84d3
Tobiasz <tobikzak@gmail.com>
2022-02-25 10:19:48 +0100

    version1

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -4,7 +4,10 @@
  * @author LC
  */
 class HelloJava {
-	public afficherCancan(){
+	public void afficherCancan(){
 
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello Blagnac");
 	}
 }
